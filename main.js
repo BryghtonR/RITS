332984a4713f2251c5e426c393d730666d68ac52
BryghtonR <bryghtonr@gmail.com>
2016-10-17 22:09:20 -0400

    Complete rework of RITS

--- main.js
+++ main.js
@@ -97,4 +97,46 @@ app.controller('FirstCtrl', function($scope, data, $routeParams){
   $scope.print = function(){
     console.log("yes");
   };
+
+  //Key Pad ----------------------------------------
+  //Clear current number
+  $scope.clearNum = function(){
+    $scope.paymentArr = [0];//use index0:0 to prevent NaN
+    $scope.paymentNum = "0.00";//initial display
+  };
+
+  $scope.clearNum();
+
+  //Converts Array of digits into correctly formatted number
+  $scope.convertNum = function(){
+    $scope.paymentNum = "";//clear
+    for(n=0;n<$scope.paymentArr.length;n++){
+      if(n==$scope.paymentArr.length-2){//places decimal point and deals with errors from using string:"."
+        $scope.paymentNum = $scope.paymentNum.concat(".");
+        $scope.paymentNum = $scope.paymentNum.concat($scope.paymentArr[n]);
+      } else {
+        $scope.paymentNum = $scope.paymentNum.concat($scope.paymentArr[n]);
+      }
+    }
+    $scope.paymentNum = parseFloat($scope.paymentNum);
+    $scope.paymentNum = $scope.paymentNum.toFixed(2);
+  };
+
+  //Concatinate Number
+  $scope.addX = function(x, y){
+    $scope.paymentArr.push(x);
+    if(y===0){//used to enable double zero key
+      $scope.paymentArr.push(y);
+    }
+    $scope.convertNum();
+  };
+
+  //Backspace
+  $scope.removeNum = function(){
+    if($scope.paymentArr.length>1){ //prevents NaN
+      $scope.paymentArr.pop();
+    }
+    $scope.convertNum();
+  };
+
 });
